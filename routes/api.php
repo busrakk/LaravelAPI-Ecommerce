3abbfd8fb60ce86033fae4fc4aac109e51ae8ad2
busrakk <busrakartal160@gmail.com>
2022-11-22 17:51:33 +0300

    added-authentication-api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,18 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+// public
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+//protected
+Route::middleware(['auth:sanctum'])->group( function(){
+    Route::get('checkAuthenticated', function(){
+        return response()->json(['message' => 'You are in', 'status'=>200], 200);
+    });
+    Route::post('logout', [AuthController::class, 'logout']);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
