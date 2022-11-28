68aec2bc1079299e9fa405f331434a883bff1939
busrakk <busrakartal160@gmail.com>
2022-11-28 14:29:33 +0300

    added-role-and-middleware

--- routes/api.php
+++ routes/api.php
@@ -19,11 +19,15 @@ use App\Http\Controllers\API\AuthController;
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
-//protected
-Route::middleware(['auth:sanctum'])->group( function(){
+// protected
+Route::middleware(['auth:sanctum', 'isAPIAdmin'])->group( function(){
     Route::get('checkAuthenticated', function(){
         return response()->json(['message' => 'You are in', 'status'=>200], 200);
     });
+    // Route::post('logout', [AuthController::class, 'logout']);
+});
+
+Route::middleware(['auth:sanctum'])->group( function(){
     Route::post('logout', [AuthController::class, 'logout']);
 });
 
